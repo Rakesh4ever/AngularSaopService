23d3048f42e41378192bdeb385ac2c19c87e561e
Rakesh4vi <rakesh4vi@gmail.com>
2022-11-12 16:38:20 +0530

    Installed Angular Material, Sum, and Substracton logic on AppComponent class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Client, NgxSoapService } from 'ngx-soap';
+import { Client, ISoapMethodResponse, NgxSoapService } from 'ngx-soap';
 
 @Component({
   selector: 'app-root',
@@ -10,13 +10,56 @@ export class AppComponent {
   title = 'AngularSaopService';
   client: Client | any;
 
+  intA!: number;
+  intB!: number;
+  loading!: boolean;
+  showDiagnostic!: boolean;
+  message!: string;
+  xmlResponse!: string;
+  jsonResponse!: string;
+  resultLabel!: string;
+ // client: Client;
+
   constructor(private soap:NgxSoapService){
-    this.soap.createClient('http://www.dneonline.com/calculator.asmx?wsdl ').then(client => this.client = client);
+    //  this.soap.createClient('http://www.dneonline.com/calculator.asmx?wsdl').then(client => this.client = client);
+   
+   this.soap.createClient('http://www.dneonline.com/calculator.asmx?wsdl')
+      .then(client => {
+        console.log('Client', client);
+        this.client = client;
+      })
+      .catch(err => console.log('Error', err));
+ }
+
+  sum() {
+    this.loading = true;
+    const body = {
+      intA: this.intA,
+      intB: this.intB
+    };
+
+    this.client.call('Add', body).subscribe((res: { responseBody: string; result: { AddResult: string; }; }) => {
+      this.xmlResponse = res.responseBody;
+      this.message = res.result.AddResult;
+      this.loading = false;
+    }, (err: any) => console.log(err));
   }
 
-  mySoap(){
-    "From component.ts"
+  subtract() {
+    this.loading = true;
+    const body = {
+      intA: this.intA,
+      intB: this.intB
+    };
+
+    (<any>this.client).Subtract(body).subscribe(
+      (res: ISoapMethodResponse) => {
+        console.log('method response', res);
+        this.xmlResponse = res.xml;
+        this.message = res.result.SubtractResult;
+        this.loading = false;
+      },
+      (      err: any) => console.log(err)
+    );
   }
-  
-  trainings=['LoanIQ','Condore','FFC','Azure Cloud'];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgxSoapModule } from 'ngx-soap';
+import {FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+const ANGULAR_MATERIAL_MODULES = [
+  MatToolbarModule, MatInputModule, MatButtonModule, MatCardModule,
+  MatProgressBarModule, MatFormFieldModule
+];
 
 @NgModule({
   declarations: [
@@ -12,7 +25,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgxSoapModule
+    NgxSoapModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    ANGULAR_MATERIAL_MODULES
    
   ],
   providers: [],
